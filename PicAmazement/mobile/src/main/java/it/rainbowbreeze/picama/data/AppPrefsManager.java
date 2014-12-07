b3464b46f89497fbbd1b0bb4381d2a4a25245b7f
rainbowbreeze <info@rainbowbreeze.it>
2014-12-07 13:28:27 +0100

    fine tuning of app settings

--- PicAmazement/mobile/src/main/java/it/rainbowbreeze/picama/data/AppPrefsManager.java
+++ PicAmazement/mobile/src/main/java/it/rainbowbreeze/picama/data/AppPrefsManager.java
@@ -8,6 +8,9 @@ import it.rainbowbreeze.picama.R;
 import it.rainbowbreeze.picama.common.ILogFacility;
 
 /**
+ *
+ * Remember: Key names have to be equal to the ones in the xml file, otherwise two different
+ *  settings are managed
  * Created by alfredomorresi on 05/12/14.
  */
 public class AppPrefsManager {
@@ -27,7 +30,16 @@ public class AppPrefsManager {
         mAppPreferences = appContext.getSharedPreferences(PREFS_FILE_NAME, 0);
     }
 
-    private static final String PREF_DROPBOXENABLED = "Pref_DropboxEnabled";
+    /**
+     * Sets default values for the preferences, given the XML file
+     */
+    public void setDefaultValues() {
+        mLogFacility.v(LOG_TAG, "Setting default preference values");
+        // See http://developer.android.com/reference/android/preference/PreferenceManager.html#KEY_HAS_SET_DEFAULT_VALUES
+        PreferenceManager.setDefaultValues(mAppContext, PREFS_FILE_NAME, Context.MODE_PRIVATE, R.xml.pref_general, false);
+    }
+
+    private static final String PREF_DROPBOXENABLED = "pref_dropboxEnabled";
     public boolean isDropboxEnabled() {
         return mAppPreferences.getBoolean(PREF_DROPBOXENABLED, false);
     }
@@ -38,15 +50,16 @@ public class AppPrefsManager {
         return this;
     }
 
-    private static final String PREF_SYNCENABLED = "Pref_SyncEnabled";
+    // Look at the xml file!
+    private static final String PREF_SYNCENABLED = "pref_enableBackgroundSync";
     public boolean isSyncEnabled() {
         return mAppPreferences.getBoolean(PREF_SYNCENABLED, false);
     }
-    public AppPrefsManager setSyncEnabled(boolean newValue) {
-        openSharedEditor();
-        mAppPreferences.getBoolean(PREF_SYNCENABLED, false);
-        saveIfNeeded();
-        return this;
+
+    // Look at the xml file!
+    private static final String PREF_SYNCFREQUENCY = "pref_syncFrequency";
+    public String getSyncFrequency() {
+        return mAppPreferences.getString(PREF_SYNCFREQUENCY, "0");
     }
 
 
@@ -78,13 +91,4 @@ public class AppPrefsManager {
         return !mSaveInBatch ? save() : false;
     }
 
-
-    /**
-     * Sets default values for the preferences, given the XML file
-     */
-    public void setDefaultValues() {
-        mLogFacility.v(LOG_TAG, "Setting default preference values");
-        // See http://developer.android.com/reference/android/preference/PreferenceManager.html#KEY_HAS_SET_DEFAULT_VALUES
-        PreferenceManager.setDefaultValues(mAppContext, PREFS_FILE_NAME, Context.MODE_PRIVATE, R.xml.pref_general, false);
-    }
 }
