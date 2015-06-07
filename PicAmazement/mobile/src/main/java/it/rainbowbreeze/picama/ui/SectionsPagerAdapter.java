3316501846bd1394953b8d7d30d8e973a6972cf8
rainbowbreeze <r@r.com>
2015-06-07 02:00:17 +0200

    switched to new TabLayout widget

---
+++ PicAmazement/mobile/src/main/java/it/rainbowbreeze/picama/ui/SectionsPagerAdapter.java
@@ -0,0 +1,51 @@
+package it.rainbowbreeze.picama.ui;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.Locale;
+
+import it.rainbowbreeze.picama.R;
+
+/**
+ * Created by alfredomorresi on 07/06/15.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+    String[] mTitles;
+
+    public SectionsPagerAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        Locale l = Locale.getDefault();
+        mTitles = new String[] {
+                context.getString(R.string.main_tabPictures).toUpperCase(l),
+                context.getString(R.string.main_tabSaved).toUpperCase(l)
+        };
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        int queryType = 0;
+
+        if (0 == position) {
+            queryType = PicturesListFragment.QUERY_VISIBLE_NOT_UPLOADED;
+        } else if (1 == position) {
+            queryType = PicturesListFragment.QUERY_UPLOADED;
+        }
+        return PicturesListFragment.newInstance(queryType);
+    }
+
+    @Override
+    public int getCount() {
+        // Show 2 total pages.
+        return 2;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mTitles[position];
+    }
+}
